476bd2d20740ac01b3dd4d4dd760605af3ea0cb6
datlass <dismadi01@gmail.com>
2020-08-23 12:41:01 +0800

    Initialize fabrik method module

---
+++ src/CodeTesting/fabriktest.lua
@@ -0,0 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local fabrik = ReplicatedStorage.Source.IKAnimation:WaitForChild("Fabrik")
+local fabrikAlgo = require(fabrik)
+
+--Module script for testing the conical constraint
+local function test()
+
+end
+
+return test
